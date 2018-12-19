0e607b9e0a9f7604462c450694979e26abd260dc
AdamRojewski <35E85c5c>
2018-12-19 21:07:51 +0100

    sdacademyTasks

--- day4/ForsExample2.java
+++ day4/ForsExample2.java
@@ -4,10 +4,10 @@ public class ForsExample2 {
     public static void main(String[] args) {
         printNumbersInTree(5);
         reverseTree(5);
-        newTree(5);
+        newTree(9);
     }
 
-    public static void printNumbersInTree(int height) {//todo odwroc choinke
+    public static void printNumbersInTree(int height) {
         for (int i = 0; i < height; i++) { // odpowiada za ilosc wierszy
             for (int j = 0; j <= i; j++) { // ilosc kolumn
                 System.out.print(i + 1);
@@ -30,18 +30,22 @@ public class ForsExample2 {
 
     public static void newTree(int wysokosc) {
         for (int i = 0; i < wysokosc; i++) {
-            for (int j = wysokosc; j > i; j--) {
+            for (int k = wysokosc; k > i; k--) {
                 System.out.print("*");
             }
-            System.out.print(i + 1);
+                for (int j = 0; j <= i; j++) {
+                    System.out.print(j + 1);
 
-            for (int j = wysokosc; j > i; j--) {
-                System.out.print("*");
+                }
+
+                for (int j = wysokosc; j > i; j--) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
-}
+
 
 
 
