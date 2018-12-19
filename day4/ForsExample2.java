55a5ee873c49ce2f166cd6a4f7e54e5ca39ff88d
AdamRojewski <35E85c5c>
2018-12-19 22:47:23 +0100

    sdacademyTasks

--- day4/ForsExample2.java
+++ day4/ForsExample2.java
@@ -1,10 +1,13 @@
 package pl.sda.javastart.day4;
 
+import java.util.Scanner;
+
 public class ForsExample2 {
     public static void main(String[] args) {
         printNumbersInTree(5);
         reverseTree(5);
-        newTree(9);
+        newTree(10);
+        fibonacci(15);
     }
 
     public static void printNumbersInTree(int height) {
@@ -33,18 +36,31 @@ public class ForsExample2 {
             for (int k = wysokosc; k > i; k--) {
                 System.out.print("*");
             }
-                for (int j = 0; j <= i; j++) {
-                    System.out.print(j + 1);
-
-                }
-
-                for (int j = wysokosc; j > i; j--) {
-                    System.out.print("*");
-                }
-                System.out.println();
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + 1);
             }
+            for (int j = wysokosc; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
+
+    public static void fibonacci(int end) {
+        int i = 0;
+        int number1 = 0;
+        int number2 = 1;
+        while (i <= end) {
+            System.out.print(number1 + " ");
+            int sumOfPrevTwo = number1 + number2;
+            number1 = number2;
+            number2 = sumOfPrevTwo;
+            i++;
+
+
+        }
+    }
+}
 
 
 
