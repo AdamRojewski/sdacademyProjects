0051bf6e6de91bf6abe61b27b5a857eddc7bf536
AdamRojewski <35E85c5c>
2018-12-19 20:51:15 +0100

    sdacademyTasks

--- day4/ForsExample2.java
+++ day4/ForsExample2.java
@@ -2,7 +2,9 @@ package pl.sda.javastart.day4;
 
 public class ForsExample2 {
     public static void main(String[] args) {
-printNumbersInTree(5);
+        printNumbersInTree(5);
+        reverseTree(5);
+        newTree(5);
     }
 
     public static void printNumbersInTree(int height) {//todo odwroc choinke
@@ -13,4 +15,34 @@ printNumbersInTree(5);
             System.out.println();
         }
     }
+
+    public static void reverseTree(int wysokosc) {
+        for (int i = 0; i < wysokosc; i++) {
+            for (int j = wysokosc; j > i; j--)
+                System.out.print(" ");
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(i + 1);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void newTree(int wysokosc) {
+        for (int i = 0; i < wysokosc; i++) {
+            for (int j = wysokosc; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.print(i + 1);
+
+            for (int j = wysokosc; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
+
+
+
+
