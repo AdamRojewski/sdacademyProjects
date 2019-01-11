7190e32a4909d998891085247014e4d640b4ee3e
AdamRojewski <35E85c5c>
2019-01-11 15:34:36 +0100

    sdacademyTasks

---
+++ day4/TankObject.java
@@ -0,0 +1,19 @@
+package pl.sda.javastart.day4;
+
+public class TankObject {
+    public static void main(String[] args) {
+        Tank tank = new Tank();
+        tank.setName("Rudy");
+        System.out.println(tank.getName());
+        tank.setCapacity(60);
+        System.out.println(tank.getCapacity());
+        tank.setFuelConsumption(12);
+        System.out.println(tank.getFuelConsumption());
+        tank.setWeight(1000);
+        System.out.println(tank.getWeight());
+
+        System.out.println(tank.toString());
+        System.out.println(tank);
+        System.out.println("Na baku przejade " + tank.howfar() + " km");
+
+}}
