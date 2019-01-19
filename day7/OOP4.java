7fca04257e6fb6f4cc50021597f324d8cdd7b44d
AdamRojewski <35E85c5c>
2019-01-19 11:14:54 +0100

    Ennum

---
+++ day7/OOP4.java
@@ -0,0 +1,29 @@
+package pl.sda.javastart.day7;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import pl.sda.javastart.day4.Car;
+
+import java.util.Arrays;
+
+public class OOP4 {
+    public static void main(String[] args) {
+        whiteCar();
+        System.out.println(Arrays.toString(ColourEnum.values()));
+        ColourEnum [] colours = ColourEnum.values();
+        for (ColourEnum x: colours) {
+            System.out.println(x.getPlName());
+        }
+        System.out.println("PONIZEJ STREAM");
+        Arrays.stream(ColourEnum.values())
+                .map(e-> e.getPlName())
+                .forEach(e-> System.out.println(e));
+
+
+        }
+
+    private static void whiteCar() {
+        Car car = new Car();
+        car.setColour(ColourEnum.WHITE);
+        System.out.println(car.getColour().getColourNumber());
+    }
+}
